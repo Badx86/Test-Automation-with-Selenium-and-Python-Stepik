3dddf6cdfa45a6cb55c5c7a962ec27e2287f5449
Badx86 <stasure@gmail.com>
2023-05-11 02:03:43 +0400

    add test_items

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def test_add_to_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    wait = WebDriverWait(browser, 10)
+    add_to_cart_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket")))
+    assert add_to_cart_button is not None, "Add to cart button is not found"
