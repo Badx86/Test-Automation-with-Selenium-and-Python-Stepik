1dec6b1ad529972cdce8d690414409bf9bbe2119
Badx86 <stasure@gmail.com>
2023-05-11 02:04:09 +0400

    change conftest for lesson_21

--- conftest.py
+++ conftest.py
@@ -1,33 +1,40 @@
-import pytest
-from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import pytest
 from selenium import webdriver
-
-
-def pytest_make_parametrize_id(config, val): return repr(val)
+import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru, en-gb, es, fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
+
+
+# @pytest.fixture(scope="function")
+# def browser(request):
+#     browser_name = request.config.getoption("browser_name")
+#     browser = None
+#     if browser_name == "chrome":
+#         print("\nstart chrome browser for test..")
+#         browser = webdriver.Chrome()
+#     elif browser_name == "firefox":
+#         print("\nstart firefox browser for test..")
+#         browser = webdriver.Firefox()
+#     else:
+#         raise pytest.UsageError("--browser_name should be chrome or firefox")
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
 
 
 # @pytest.fixture(scope="function")
