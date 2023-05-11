b402ab4c96304334b7dc6b3bf290a68a767af5d1
Badx86 <stasure@gmail.com>
2023-05-12 02:38:48 +0400

    add login_page assert

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,6 +10,7 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, "Login URL is not correct"
+
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
         assert True
