3ebac2e290220778faa2167bbe60a868cf42ec82
Badx86 <stasure@gmail.com>
2023-05-12 01:56:40 +0400

    change fixture

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium import webdriver
 import pytest
-
 
 
 def pytest_addoption(parser):
@@ -13,44 +10,14 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
+    # В переменную user_language передается параметр из командной строки
     user_language = request.config.getoption("language")
+    # Инициализируются опции браузера
     options = Options()
+    # В опции вебдрайвера передаем параметр из командной строки
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     yield browser
     browser.quit()
 
-
-# @pytest.fixture(scope="function")
-# def browser(request):
-#     browser_name = request.config.getoption("browser_name")
-#     browser = None
-#     if browser_name == "chrome":
-#         print("\nstart chrome browser for test..")
-#         browser = webdriver.Chrome()
-#     elif browser_name == "firefox":
-#         print("\nstart firefox browser for test..")
-#         browser = webdriver.Firefox()
-#     else:
-#         raise pytest.UsageError("--browser_name should be chrome or firefox")
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
-
-
-# @pytest.fixture(scope="function")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome()
-#     browser.implicitly_wait(30)
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
-
-
-# @pytest.mark.parametrize('language', ["ru", "en-gb"])
-# def test_guest_should_see_login_link(browser, language):
-#     link = f"http://selenium1py.pythonanywhere.com/{language}/"
-#     browser.get(link)
-#     browser.find_element(By.CSS_SELECTOR, "#login_link")
