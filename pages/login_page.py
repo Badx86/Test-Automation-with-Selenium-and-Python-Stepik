c93d3e8b086440ec1e04f947cdc78d851d9d8d19
Badx86 <stasure@gmail.com>
2023-05-12 02:53:29 +0400

    add login and registration asserts

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -13,8 +14,9 @@ class LoginPage(BasePage):
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_SUBMIT), "Submit button is present on the page"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_SUBMIT), "Submit button is present on the page"
+
