fc8e1d9b5abe3362b2c888b57caf8d247fe8e126
Badx86 <stasure@gmail.com>
2023-05-12 01:57:28 +0400

    add comments

---
+++ test_main_page.py
@@ -0,0 +1,17 @@
+from pages.main_page import MainPage
+
+# pytest -v --tb=line --language=en test_main_page.py
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+
+
+
+
+
