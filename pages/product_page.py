329d68488c74c65bb9d73fb56288f7960e2a6c9d
Badx86 <stasure@gmail.com>
2023-05-12 16:28:02 +0400

    add product_page file and class

---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+import math
+from selenium.common import NoAlertPresentException
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
+        add_to_basket_btn.click()
+
+    def should_be_present_msg(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MSG), "Success message is not present"
+
+    def should_be_basket_total(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL_MSG), "Basket total message is not present"
+
+
