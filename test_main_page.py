34046a02250267be33bf241186c028c3b43cc85f
Badx86 <stasure@gmail.com>
2023-05-12 19:53:17 +0400

    fix code and comments

--- test_main_page.py
+++ test_main_page.py
@@ -12,7 +12,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     # выполняем метод страницы — переходим на страницу логина
     page.go_to_login_page()
-    # login_page = LoginPage(browser, browser.current_url)
     page.should_be_login_link()
 
 
@@ -21,8 +20,4 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-
-
 
