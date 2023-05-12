85445f137af84a95927a5a4e66bce70409fb6094
Badx86 <stasure@gmail.com>
2023-05-12 19:52:09 +0400

    add product_name

--- test_product_page.py
+++ test_product_page.py
@@ -5,8 +5,10 @@ def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
+    product_name = page.get_product_name()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_success_msg()
+    assert product_name == page.get_success_message_product_name(), "Product name does not match"
     page.should_be_basket_total()
 
