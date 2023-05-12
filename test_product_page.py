864eafb69b2052ed8d9001525d8ed8a04753278f
Badx86 <stasure@gmail.com>
2023-05-12 18:58:42 +0400

    fix grammar should_be_success_msg

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,3 @@
-import math
-from selenium.common.exceptions import NoAlertPresentException
 from pages.product_page import ProductPage
 
 
@@ -9,6 +7,6 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_success_message()
+    page.should_be_success_msg()
     page.should_be_basket_total()
 
