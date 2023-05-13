62833c438830ee2df326bb01e51b54fb7b62feaf
Badx86 <stasure@gmail.com>
2023-05-14 01:50:01 +0400

    add TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,27 @@
+import pytest
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.basket_page import BasketPage
 # pytest -v --tb=line --language=en test_main_page.py
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page = MainPage(browser, link)
-    # открываем страницу
-    page.open()
-    # выполняем метод страницы — переходим на страницу логина
-    page.go_to_login_page()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = MainPage(browser, link)
+        # открываем страницу
+        page.open()
+        # выполняем метод страницы — переходим на страницу логина
+        page.go_to_login_page()
+        page.should_be_login_link()
 
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
