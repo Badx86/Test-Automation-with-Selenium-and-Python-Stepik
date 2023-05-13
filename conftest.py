41b62e0d416958a83926e80f4020aa2715c1b8dc
Badx86 <stasure@gmail.com>
2023-05-13 05:31:55 +0400

    comment product_page fixture

--- conftest.py
+++ conftest.py
@@ -23,9 +23,9 @@ def browser(request):
     browser.quit()
 
 
-@pytest.fixture(scope="function")
-def product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.open()
-    return page
+# @pytest.fixture(scope="function")
+# def product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     return page
