c411b700f98c47dda4a458cbf062c8cd46cdfca7
Badx86 <stasure@gmail.com>
2023-05-14 02:25:00 +0400

    add register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,16 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL_INPUT)
+        email_input.send_keys(email)
+        password_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_INPUT)
+        password_input.send_keys(password)
+        confirm_password_input = self.browser.find_element(*LoginPageLocators.REGISTRATION_CONFIRM_PASSWORD_INPUT)
+        confirm_password_input.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTRATION_SUBMIT)
+        register_button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
