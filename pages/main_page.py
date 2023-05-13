3c8427554f4b70ac55c8b576786390a49dd0022d
Badx86 <stasure@gmail.com>
2023-05-13 20:20:07 +0400

    fix basket_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,14 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .basket_page import BasketPage
+from .locators import MainPageLocators, BasketPageLocators
 from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
+
+    def go_to_basket_page(self):
+        basket_link = self.browser.find_element(*BasketPageLocators.BASKET_LINK)
+        basket_link.click()
+        return BasketPage(self.browser, self.browser.current_url)
