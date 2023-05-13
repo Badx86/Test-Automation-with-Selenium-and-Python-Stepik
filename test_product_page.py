60b0840f9a357bc58c05ac7c2f7dc817a71cea2b
Badx86 <stasure@gmail.com>
2023-05-14 02:25:55 +0400

    add class TestUserAddToBasketFromProductPage and rename its functions from guest to user

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,24 @@ from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 
 
+class TestUserAddToBasketFromProductPage():
+    def test_user_can_add_product_to_basket(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        product_name = page.get_product_name()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_msg()
+        success_message_product_name = page.get_success_message_product_name()
+        assert product_name in success_message_product_name, "Product names do not match"
+        page.should_be_basket_total()
+
+    def test_user_cant_see_success_message(self, browser, link):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -12,35 +30,17 @@ from pages.product_page import ProductPage
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                 marks=pytest.mark.xfail),
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
 ])
-def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
-    product_name = page.get_product_name()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_success_msg()
-    success_message_product_name = page.get_success_message_product_name()
-    assert product_name in success_message_product_name, "Product names do not match"
-    page.should_be_basket_total()
-
-
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.should_not_be_success_message()
-
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.open()
     page.should_not_be_success_message()
 
 
