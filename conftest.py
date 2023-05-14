2e88929bc07ff8cc24fcd414178cb6f5233fbdcd
Badx86 <stasure@gmail.com>
2023-05-14 17:48:57 +0400

    delete unusable commented fixtures

--- conftest.py
+++ conftest.py
@@ -1,9 +1,6 @@
-import time
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import pytest
-from pages.login_page import LoginPage
-from pages.product_page import ProductPage
 
 
 def pytest_addoption(parser):
@@ -23,22 +20,3 @@ def browser(request):
     browser.maximize_window()
     yield browser
     browser.quit()
-
-
-# @pytest.fixture(scope="function")
-# def setup(browser):
-#     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-#     page = LoginPage(browser, link)
-#     page.open()
-#     email = str(time.time()) + "@fakemail.org"
-#     password = "test_password"
-#     page.register_new_user(email, password)
-#     page.should_be_authorized_user()
-
-
-# @pytest.fixture(scope="function")
-# def product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     return page
