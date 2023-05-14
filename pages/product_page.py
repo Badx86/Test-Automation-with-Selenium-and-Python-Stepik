32a8470ed07432af4e1a7421ba65777ad95ae207
Badx86 <stasure@gmail.com>
2023-05-14 16:49:32 +0400

    add func

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,3 @@
-from itertools import product
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import ProductPageLocators, BasketPageLocators
 from .basket_page import BasketPage
@@ -22,13 +20,11 @@ class ProductPage(BasePage):
         return product_name_element.text
 
     def get_product_price(self):
-        product_price_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text.strip()
-        return product_price_element
+        product_price_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return product_price_element.text.strip()
 
     def get_success_message_product_name(self):
         success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MSG).text
-        expected_message = "Coders at Work has been added to your basket."
-        assert success_message == expected_message, "Unexpected success message: {}".format(success_message)
         product_name = success_message.split(" has been added to your basket.")[0]
         return product_name
 
@@ -44,3 +40,21 @@ class ProductPage(BasePage):
         basket_link = self.browser.find_element(*BasketPageLocators.BASKET_LINK)
         basket_link.click()
         return BasketPage(self.browser, self.browser.current_url)
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BTN), "Add to basket button is not present"
+
+    def should_be_product_name_in_success_message(self):
+        product_name_in_msg = self.get_success_message_product_name()
+        actual_product_name = self.get_product_name()
+        assert product_name_in_msg == actual_product_name, "Product name in success message does not match actual product name"
+
+    def get_basket_total_message_product_price(self):
+        basket_total_message = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_MSG).text
+        product_price_in_basket_total = basket_total_message.split()[-1]
+        return product_price_in_basket_total
+
+    def should_be_product_price_in_basket_total(self):
+        product_price_in_basket_total = self.get_basket_total_message_product_price()
+        actual_product_price = self.get_product_price()
+        assert product_price_in_basket_total == actual_product_price, "Product price in basket total does not match actual product price"
