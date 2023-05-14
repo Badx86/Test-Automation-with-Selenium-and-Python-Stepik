b57e82ac713afe60b16a6e9d1c2ef95793931999
Badx86 <stasure@gmail.com>
2023-05-14 16:48:35 +0400

    add test_guest_can_add_product_to_basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 import pytest
-from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.basket_page import BasketPage
+from pages.product_page import ProductPage
 # pytest -v --tb=line --language=en test_main_page.py
 
 
@@ -32,5 +32,18 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket_message()
     basket_page.should_not_be_items_in_basket()
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_msg()
+    page.should_be_product_name_in_success_message()
+    page.should_be_basket_total()
+    page.should_be_product_price_in_basket_total()
+
 
 
