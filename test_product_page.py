f3a9b81f3bf1eb5424de6bcbde817f49df1183b0
Badx86 <stasure@gmail.com>
2023-05-14 17:47:04 +0400

    marked the tests required for the review

--- test_product_page.py
+++ test_product_page.py
@@ -25,6 +25,7 @@ def test_guest_should_see_login_link_on_product_page(browser, link):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -32,6 +33,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -40,6 +42,17 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket_message()
     basket_page.should_not_be_items_in_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_button()
+    page.add_product_to_basket()
+    page.should_be_product_name_in_success_message()
+    page.should_be_product_price_in_basket_total()
 
 
 class TestUserAddToBasketFromProductPage:
@@ -53,6 +66,7 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
